f8111684d4ae9064729002dcf77af7dc2a633634
Kongo <kongo555@gmail.com>
2020-03-21 12:57:46 +0100

    Add Api::V1 namespace to UsersController

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def create
+        User.create(params[:user]).send_registration_email
+      end
+
+      def update
+        User.find(params[:id]).update(params[:user])
+      end
+
+      def delete
+        User.delete(params[:id])
+      end
+    end
+  end
+end
