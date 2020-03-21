7de3903a923e5cc8dcf03f46052845a94711fa27
Kongo <kongo555@gmail.com>
2020-03-21 20:28:43 +0100

    Add rescue_from RecordNotFound and ParameterMissing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActionController::ParameterMissing, with: :render_bad_request_response
+
+  def render_not_found_response(error)
+    render json: { errors: { base: [error.message] } }, status: :not_found
+  end
+
+  def render_bad_request_response(error)
+    render json: { errors: { base: [error.message] } }, status: :unprocessable_entity
+  end
 end
